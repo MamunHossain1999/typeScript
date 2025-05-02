7d38af35aa45fb226e92e775b82a69b73eacccc7
Your Name <youremail@example.com>
2025-05-02 21:48:12 +0600

    generic type practice

---
+++ day-1/src/day-2/genericsType.ts
@@ -0,0 +1,64 @@
+{
+    //generics type 
+
+    //avabe dynamic kore
+type GenericArray<T> = Array<T>; // <> ai chinho diye generic type defind kora hoy 
+// T mane holo parameter
+
+// const rollNumbers: number[] = [1,2,3]; normal type
+const rollNumbers: GenericArray<number> = [1,2,3]; //generic type
+ 
+// const mentors: string[] = ['m','n','d'];
+const mentors : GenericArray<string> = ['m','n','d'];
+
+// const boleArray : boolean[] = [true, false, true]'
+const boleArray : GenericArray<boolean> = [true, false, true];
+
+
+
+const user : GenericArray<{name:string, age:number}> = [
+    {
+        name: 'mamun',
+        age: 34,
+    },
+    {
+        name: 'jm',
+        age: 234
+    }
+]
+
+
+const add = (x:number, y:number)=> x+y;
+add(30,34);  //optional
+
+
+//generic tuple
+
+type GenericTuple<x,y> = [x,y];
+
+const manush : GenericTuple<string, string> = ['mm','hd'];
+
+const UserWithId : GenericTuple<number,{name:string, email:string}> = [133, {name:'am', email:'m@gmail.com'}]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
